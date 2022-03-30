26165fdc3960b64fae65b82b27376ad38d27bccd
Nikolai Kochubeev <nikolay.kochubeev@gmail.com>
2022-03-30 20:50:44 +0300

    chore: optimize dichotomy

--- Laboratory 1/main.py
+++ Laboratory 1/main.py
@@ -1,24 +1,31 @@
-from math import sin
+from math import *
 
 
 class Optimization:
-    def __init__(self, function):
+    def __init__(self, function, a, b, epsilon):
         self.function = function
+        self.a = a
+        self.b = b
+        self.epsilon = epsilon
 
-    def calculate_dichotomy(self, a, b):
-        epsilon = 1e-5
-        while b - a > epsilon:
-            x1 = (a + b) / 2 - epsilon / 3
-            x2 = (a + b) / 2 + epsilon / 3
-            if self.function(x1) < self.function(x2):
-                b = x2
-            elif self.function(x1) > self.function(x2):
-                a = x1
+    def calculate_dichotomy(self):
+        n = 0
+        result = 0
+        a = self.a
+        b = self.b
+        fa = self.function(a)
+        fb = self.function(b)
+        while (b - a) / 2 > self.epsilon:
+            result = (a + b) / 2
+            if fa <= fb:
+                b = result
+                fb = self.function(b)
             else:
-                a = x1
-                b = x2
-        print((a + b) / 2)
+                a = result
+                fa = self.function(a)
+            n = n + 1
+        print(result, n)
 
 
-optimization = Optimization(lambda x: sin(x) * x ** 2)
-optimization.calculate_dichotomy(-3, -2)
+optimization = Optimization(lambda x: sin(x) * x ** 2, -3, -2, 1e-5)
+optimization.calculate_dichotomy()
