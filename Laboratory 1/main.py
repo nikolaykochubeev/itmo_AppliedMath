f8daa3a8e6cd1e24afb1b9dbb2b74cb751e1007a
vaner29 <gleb.mihailichencko@gmail.com>
2022-03-30 23:31:20 +0300

    feat: add parabola method

--- Laboratory 1/main.py
+++ Laboratory 1/main.py
@@ -1,6 +1,9 @@
 from math import *
 
+import math
 import numpy as np
+import matplotlib.pyplot as plt
+from numpy.linalg.linalg import LinAlgError
 
 
 class Optimization:
@@ -34,6 +37,44 @@ class Optimization:
         x = (a + b) / 2
         print('x = ', x)
         print('f(x) = ', self.function(x))
+        print('n = ', n)
+
+    def calculate_parabola(self):
+        x1 = self.a
+        x3 = self.b
+        x2 = (x3 - x1) / 2
+        x_i = 0
+        n = 0
+        f1 = self.function(x1)
+        f2 = self.function(x2)
+        f3 = self.function(x3)
+        while True:
+            u = x2 - ((x2 - x1) ** 2 * (f2 - f3) - (x2 - x3) ** 2 * (f2 - f1)) / (
+                        2 * ((x2 - x1) * (f2 - f3) - (x2 - x3) * (f2 - f1)))
+            fu = self.function(u)
+            if fu <= f2:
+                if u < x2:
+                    x3 = x2
+                    f3 = f2
+                else:
+                    x1 = x2
+                    f1 = f2
+                x2 = u
+                f2 = fu
+            else:
+                if x2 < u:
+                    x3 = u
+                    f3 = fu
+                else:
+                    x1 = u
+                    f1 = fu
+            if abs(x_i - u) < self.epsilon:
+                break
+            x_i = u
+            n = n + 1
+
+        print('x = ', u)
+        print('f(x) = ', self.function(u))
         print('n = ', n)
 
     def calculate_brent_method(self):
@@ -114,6 +155,7 @@ class Optimization:
 
 optimization = Optimization(lambda x: sin(x) * x ** 2, -3, -2, 1e-5)
 optimization.calculate_dichotomy()
+optimization.calculate_parabola()
 optimization.calculate_brent_method()
 
 # метод Брента комбинирует метод золотого сечения и парабол
