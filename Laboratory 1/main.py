fcf51108f8d19e741a8465c4db2a792c1bacff0d
Nikolai Kochubeev <nikolay.kochubeev@gmail.com>
2022-03-30 13:58:25 +0300

    chore: organized into class

--- Laboratory 1/main.py
+++ Laboratory 1/main.py
@@ -1,23 +1,24 @@
 from math import sin
 
 
-def f(x):
-    return sin(x) * x ** 2
+class Optimization:
+    def __init__(self, function):
+        self.function = function
+
+    def calculate_dichotomy(self, a, b):
+        epsilon = 1e-5
+        while b - a > epsilon:
+            x1 = (a + b) / 2 - epsilon / 3
+            x2 = (a + b) / 2 + epsilon / 3
+            if self.function(x1) < self.function(x2):
+                b = x2
+            elif self.function(x1) > self.function(x2):
+                a = x1
+            else:
+                a = x1
+                b = x2
+        print((a + b) / 2)
 
 
-def calculate_dichotomy(a, b):
-    epsilon = 1e-5
-    while b - a > epsilon:
-        x1 = (a + b) / 2 - epsilon / 3
-        x2 = (a + b) / 2 + epsilon / 3
-        if f(x1) < f(x2):
-            b = x2
-        elif f(x1) > f(x2):
-            a = x1
-        else:
-            a = x1
-            b = x2
-    print((a + b) / 2)
-
-
-calculate_dichotomy(-3, -2)
+optimization = Optimization(lambda x: sin(x) * x ** 2)
+optimization.calculate_dichotomy(-3, -2)
