0406dfb2473f2ac829c3192240b2d5feb2fe9363
nikolaykochubeev <nikolay.kochubeev@gmail.com>
2022-05-20 23:18:21 +0300

    chore: complete

--- Laboratory_3/conditional_number.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-from reverse import reverse
-
-
-def get_conditional_number(A):
-    A_reverse = reverse(A).todense()
-    A_reverse = np.squeeze(np.asarray(A_reverse))
-    return np.linalg.norm(A) * np.linalg.norm(A_reverse)
--- Laboratory_3/test_matrix.py
+++
@@ -1,7 +0,0 @@
-from LU_Solution import solve
-from systems_solver import solve_systems, generate_gilbert_matrix, generate_diagonal_matrix
-from method_Zeldel import solution
-from print_Matrix import print_matrix
-
-solve_systems(generate_gilbert_matrix, solution, 14)
-solve_systems(generate_diagonal_matrix, solution, 14)
